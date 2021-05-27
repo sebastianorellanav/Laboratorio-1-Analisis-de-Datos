5c705092aefe0c06039f3c5b0f4637d10d251c61
Gary Simken <gary.simken@usach.cl>
2021-05-26 21:57:52 -0400

    Final push

--- main2.r
+++ main2.r
@@ -55,157 +55,62 @@ df$class <- as.character(df$class)
 df$class[df$class == "2"] <- "Benigno"
 df$class[df$class == "4"] <- "Maligno"
 
-# #Se define una función para comparar cada variable con la clase de cancer que se tiene (benigno o maligno)
-# grafico <- function(df,aes,y_string){
-#   boxplt =  ggboxplot(data = df, x = "class", y = y_string, color = "class",) + border()
-# 
-#   ydens = axis_canvas(boxplt, axis = "y", coord_flip = TRUE) + geom_density(data = df, aes, alpha = 0.7, size = 0.2) + coord_flip()
-# 
-#   boxplt = insert_yaxis_grob(boxplt, ydens, grid::unit(.2, "null"), position = "right")
-#   ggdraw(boxplt)
-# }
-# #y se llama para cada columna
-# a<-grafico(df,aes(x = clumpThickness, fill = class),"clumpThickness")
-# b<-grafico(df,aes(x = uniformityCellSize, fill = class),"uniformityCellSize")
-# c<-grafico(df,aes(x = uniformityCellShape, fill = class),"uniformityCellShape")
-# d<-grafico(df,aes(x = marginalAdhesion, fill = class),"marginalAdhesion")
-# e<-grafico(df,aes(x = singleEpithCellSize, fill = class),"singleEpithCellSize")
-# f<-grafico(df,aes(x = bareNuclei, fill = class),"bareNuclei")
-# g<-grafico(df,aes(x = blandChromatin, fill = class),"blandChromatin")
-# h<-grafico(df,aes(x = normalNucleoli, fill = class),"normalNucleoli")
-# i<-grafico(df,aes(x = mitoses, fill = class),"mitoses")
-# ggarrange(a,b,c,d)
-# ggarrange(e,f,g,h)
-# i
-# 
-# 
-# #Al revisar minuciosamente se llega al concenso de que hay que filtrar
-# #ciertos datos
-# 
-# df1<-df[!(((df$uniformityCellSize>5 | df$uniformityCellShape>5 | df$marginalAdhesion>7 | df$singleEpithCellSize>7 |
-#               df$bareNuclei>6 | df$blandChromatin>6 | df$normalNucleoli>7 | df$mitoses>6)
-#            & df$class =="Benigno")  | (df$mitoses>6 & df$class =="Maligno")),]
-# 
-# df1<-df1[!is.na(df1$clumpThickness),]
-# 
-# # y se vuelven a graficar
-# a2<-grafico(df1,aes(x = clumpThickness, fill = class),"clumpThickness")
-# b2<-grafico(df1,aes(x = uniformityCellSize, fill = class),"uniformityCellSize")
-# c2<-grafico(df1,aes(x = uniformityCellShape, fill = class),"uniformityCellShape")
-# d2<-grafico(df1,aes(x = marginalAdhesion, fill = class),"marginalAdhesion")
-# e2<-grafico(df1,aes(x = singleEpithCellSize, fill = class),"singleEpithCellSize")
-# f2<-grafico(df1,aes(x = bareNuclei, fill = class),"bareNuclei")
-# g2<-grafico(df1,aes(x = blandChromatin, fill = class),"blandChromatin")
-# h2<-grafico(df1,aes(x = normalNucleoli, fill = class),"normalNucleoli")
-# i2<-grafico(df1,aes(x = mitoses, fill = class),"mitoses")
-# ggarrange(a2,b2,c2,d2)
-# ggarrange(e2,f2,g2,h2)
-# i2
+
 
 pruebas = df
 pruebas$id <- NULL
 pruebas$class<- NULL
-
 datos.long<-melt(pruebas)
-p2 <- ggqqplot(
-  datos.long,
-  x = "value",
-  color = "variable"
-)
-p2 <- p2 + facet_wrap(~ variable)
-print(p2)
-
-p1 <- ggboxplot(
+bp1 <- ggboxplot(
   datos.long,
   x = "variable", y = "value",
   fill = "variable"
 )
-print(p1)
+print(bp1)
 
 df$marginalAdhesion[df$marginalAdhesion > 5] <- mean(df$marginalAdhesion)
 df$singleEpithCellSize[df$singleEpithCellSize > 4] <- mean(df$singleEpithCellSize)
 df$blandChromatin[df$blandChromatin > 4] <- mean(df$blandChromatin)
 df$normalNucleoli[df$normalNucleoli > 5] <- mean(df$normalNucleoli)
 df$mitoses[df$mitoses > 2] <- mean(df$mitoses)
-#df<-df[!(df$marginalAdhesion>8 | df$singleEpithCellSize>7 |
-#          df$blandChromatin>9 | df$normalNucleoli>8 | df$mitoses>8),]
-
-#df<-df[!is.na(df1$clumpThickness),]
 
 pruebas = df
 pruebas$id <- NULL
 pruebas$class<- NULL
 
 datos.long2<-melt(pruebas)
-p2 <- ggqqplot(
+qq2 <- ggqqplot(
   datos.long2,
   x = "value",
   color = "variable"
 )
-p2 <- p2 + facet_wrap(~ variable)
-#print(p2)
+qq2 <- qq2 + facet_wrap(~ variable)
+print(qq2)
 
-p1 <- ggboxplot(
+bp2 <- ggboxplot(
   datos.long2,
   x = "variable", y = "value",
   fill = "variable"
 )
-print(p1)
+print(bp2)
 
-
-distribucion <- apply(pruebas,2,shapiro.test)
-par(mfrow=c(3,3))
-#en cada uno de las columnas el p-valor es menor a 0.05, por lo que rechazamos en todas
-#Para verificar se revisara el histograma de cada columna (En caso de error, agrandar la seccion de graficos)
-ign<-mapply(hist,pruebas,main=colnames(pruebas),col="lightsteelblue",xlab="Puntaje")
-
-
-# #Aqui estará la cantidad de datos que se trabajará
-# B = sum(df1$class == "Benigno",na.rm=TRUE)
-# M = sum(df1$class == "Maligno",na.rm=TRUE)
-# Tot = B+M
-# PB= B/Tot
-# PM= M/Tot
-# cat("Totales:",Tot,"\nBenignos:",B,"Malignos:",M,"\nPorcentaje Benigno:",PB,"Porcentaje Maligno:",PM)
-
-# # GRAFICO CIRCULAR para revisar la distribucion de estos
-# valores <- c(PB, PM)
-# etiquetas <- c("Benignos","Malignos")
-# etiquetas <- paste(etiquetas,round(valores,2))
-# etiquetas <- paste(etiquetas,"%",sep="")
-# pie(valores, ,labels = etiquetas,col=rainbow(2), main="% Casos Benignos vs Malignos")
-
-#Para calcular las medidas de centralizaciony dispersion se sacara id y class
-
+#Ahora con los datos limpios calculamos lo siguiente
 medias = apply(pruebas, 2, mean)
 medianas =  apply(pruebas, 2, median)
 modas = apply(pruebas, 2, mlv,  method = "mfv")
 desviaciones = apply(pruebas, 2, sd)
 varianzas = apply(pruebas, 2, var)
 rangos = apply(pruebas, 2, range)
+
 #Se crea una tabla para mostrar todo
 tabla = matrix(c(medias,medianas,modas,varianzas,desviaciones),ncol=9,byrow=TRUE)
 #Se le asigna nombre a las columnas
-colnames(tabla) =  c("clumpThickness",
-                     "uniformityCellSize",
-                     "uniformityCellShape",
-                     "marginalAdhesion",
-                     "singleEpithCellSize",
-                     "bareNuclei",
-                     "blandChromatin",
-                     "normalNucleoli",
-                     "mitoses"
-)
+colnames(tabla) =  c("clumpThickness","uniformityCellSize","uniformityCellShape","marginalAdhesion","singleEpithCellSize","bareNuclei","blandChromatin","normalNucleoli","mitoses")
 #y a las filas
-rownames(tabla) = c("media",
-                    "mediana",
-                    "moda",
-                    "varianza",
-                    "SD"
-)
+rownames(tabla) = c("media","mediana","moda","varianza","SD")
 #Finalmente se convierte a tabla y se muestra
 tabla=as.table(tabla)
-tabla
+print(tabla)
 
 
 
@@ -220,17 +125,64 @@ par(mfrow=c(3,3))
 #en cada uno de las columnas el p-valor es menor a 0.05, por lo que rechazamos en todas
 #Para verificar se revisara el histograma de cada columna (En caso de error, agrandar la seccion de graficos)
 ign<-mapply(hist,pruebas,main=colnames(pruebas),col="lightsteelblue",xlab="Puntaje")
-
-
 #Efectivamente, Ninguna sigue una distribuición normal, por lo que se opta por un test no paramétrico
 
-
+#se realiza una prueba de spearman
 df.cor = cor(pruebas, method = 'spearman')
 #Para luego realizar un grafico de calor con la matriz de correlacion
-ggcorrplot(df.cor, 
-           hc.order = TRUE, 
-           type = "full",
-           lab = TRUE)
+ggcorrplot(df.cor,hc.order = TRUE,type = "full",lab = TRUE)
+
+
+#Aqui estará la cantidad de datos que se trabajará
+B = sum(df$class == "Benigno",na.rm=TRUE)
+M = sum(df$class == "Maligno",na.rm=TRUE)
+Tot = B+M
+PB= B/Tot
+PM= M/Tot
+cat("Totales:",Tot,"\nBenignos:",B,"Malignos:",M,"\nPorcentaje Benigno:",PB,"Porcentaje Maligno:",PM)
+#GRAFICO CIRCULAR para revisar la distribucion de estos
+valores <- c(PB, PM)
+etiquetas <- c("Benignos","Malignos")
+etiquetas <- paste(etiquetas,round(valores,2))
+etiquetas <- paste(etiquetas,"%",sep="")
+pie(valores, ,labels = etiquetas,col=rainbow(2), main="% Casos Benignos vs Malignos")
+
+
+#Se define una función para comparar cada variable con la clase de cancer que se tiene (benigno o maligno)
+grafico <- function(df,aes,y_string){
+  boxplt =  ggboxplot(data = df, x = "class", y = y_string, color = "class",) + border()
+
+  ydens = axis_canvas(boxplt, axis = "y", coord_flip = TRUE) + geom_density(data = df, aes, alpha = 0.7, size = 0.2) + coord_flip()
+
+  boxplt = insert_yaxis_grob(boxplt, ydens, grid::unit(.2, "null"), position = "right")
+  ggdraw(boxplt)
+}
+#y se llama para cada columna
+a<-grafico(df,aes(x = clumpThickness, fill = class),"clumpThickness")
+b<-grafico(df,aes(x = uniformityCellSize, fill = class),"uniformityCellSize")
+c<-grafico(df,aes(x = uniformityCellShape, fill = class),"uniformityCellShape")
+d<-grafico(df,aes(x = marginalAdhesion, fill = class),"marginalAdhesion")
+e<-grafico(df,aes(x = singleEpithCellSize, fill = class),"singleEpithCellSize")
+f<-grafico(df,aes(x = bareNuclei, fill = class),"bareNuclei")
+g<-grafico(df,aes(x = blandChromatin, fill = class),"blandChromatin")
+h<-grafico(df,aes(x = normalNucleoli, fill = class),"normalNucleoli")
+i<-grafico(df,aes(x = mitoses, fill = class),"mitoses")
+ggarrange(a,b,c,d)
+ggarrange(e,f,g,h)
+i
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
